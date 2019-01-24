4a39851c5b860e282f5214ade2ce9e8295727d71
bill-baumgartner <bill.baumgartner@gmail.com>
2019-01-24 05:04:02 -0700

    Squash - whitespace change only

--- project.clj
+++ project.clj
@@ -2,19 +2,19 @@
   :description "knowledge representation and reasoning tools"
   :url "https://github.com/UCDenver-ccp/kr"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-           [org.clojure/java.classpath "0.3.0"]
-           [org.openrdf.sesame/sesame-runtime "2.7.12"]
-           [org.apache.jena/jena-arq "2.10.1"]
-           [log4j/log4j "1.2.17"]
-           [commons-logging/commons-logging "1.1.1"]
-           [commons-codec/commons-codec "1.6"]
-           [com.stuartsierra/dependency "0.1.1"]
-           [org.slf4j/slf4j-log4j12 "1.7.2"]
-           [junit/junit "3.8.1"]]
+                 [org.clojure/java.classpath "0.3.0"]
+                 [org.openrdf.sesame/sesame-runtime "2.7.12"]
+                 [org.apache.jena/jena-arq "2.10.1"]
+                 [log4j/log4j "1.2.17"]
+                 [commons-logging/commons-logging "1.1.1"]
+                 [commons-codec/commons-codec "1.6"]
+                 [com.stuartsierra/dependency "0.1.1"]
+                 [org.slf4j/slf4j-log4j12 "1.7.2"]
+                 [junit/junit "3.8.1"]]
   :main nil
-  :source-paths ["src" "test"] ;; figure out how to make a test-jar using a profile
+  :source-paths ["src" "test"]                              ;; figure out how to make a test-jar using a profile
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
 
